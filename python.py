a8efd54b6413187a674ed42bb4ca8041d661b368
parineet kaur ahuja <parineetdataanalytics@gmail.com>
2024-06-20 13:12:09 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,87 @@
+
+'''print("hello world")
+
+x="india"
+
+print(x)
+
+
+x,y=100,200
+
+print(x,y)
+
+y=100
+q=1000
+a,b=200,500
+
+print(y,q)
+
+print(a)
+print(b)
+
+x = 500
+y = 5
+
+print("multiplication", x*y)
+print("addition",x+y )
+print("substraction",x-y)
+
+ab=x*y
+
+print("parineet",ab)
+
+print("ashuu",ab)
+
+pc=x+y
+print(pc)'''
+
+'''x="india"
+print(x)
+'''
+'''x=15000
+y=500
+
+print(x,y)
+print(y)
+'''
+
+'''
+x=120.79
+
+a=str(x)
+print(type(x))
+print(len(x))
+
+
+operators :- types of operators
+
+1. Airthmatic operators
+2.Assignment operators
+3.comparison operators
+4.lofical operators
+5.identity operators
+6.Membership operators
+7.Bitwise operators '''
+
+
+'''Airthmatic operators :- 
++ :addition,
+- :substraction,
+* :multiplication,
+/ :division,
+% :module - It shows reminder 
+** :exponentation - it is used to add the power on number.
+//:floor division - it shows the decimal before decimal
+'''
+
+# x=45
+# y=20
+
+# print("modules:-",x%y)
+
+# print "the reminder of ",x,"and",y,"is:",x%y)
+
+#----------------------------------------------------------------
+
+print("hello world")
+print("welcome to python classes")
